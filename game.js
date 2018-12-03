ab81af9c0e6e23d2a91929ae838e5bc4601f0a17
Junil Ko <goyadb@outlook.com>
2018-12-03 14:11:27 +0900

    socket.io 초기화 구현

---
+++ game.js
@@ -0,0 +1,15 @@
+
+module.exports = function(server) {
+
+    var io = require('socket.io')(server, {
+        transports: ['websocket'],
+    });
+
+    io.on('connection', function(socket) {
+        console.log('Connected: ' + socket.id);
+
+        socket.on('disconnect', function(reson) {
+            console.log('Disconnected: ' + socket.id);
+        });
+    });
+};
